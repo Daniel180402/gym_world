819df5952ff1b52cc8f7fb72f18b483e5f6abf02
Daniel Jarnig <danieljarnig1804@gmail.com>
2023-06-08 11:14:12 +0200

    Added basic components for the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
+import HeroBanner from '../componets/HeroBanner';
+import SearchExercises from '../componets/SearchExercises';
+import Exercises from '../componets/Exercises';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <Box>
+            <HeroBanner />
+            <SearchExercises />
+            <Exercises />
+        </Box>
+    )
 }
 
 export default Home
